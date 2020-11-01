4b47b4d1dec4f6e92ad30d4ee5d182e9eb4eb1fe
Julien Giovaresco <dev@giovaresco.fr>
2020-11-01 16:44:56 +0100

    chore: add react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+
+const App = () => (
+  <div>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron / React application.</p>
+  </div>
+);
+
+export default hot(module)(App);
